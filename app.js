12f5b83b8938dc39fa2b7d60235102a0a8c5998a
Frances Saliwo <77273506+fs-al@users.noreply.github.com>
2023-04-09 18:59:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.post("/", function(req, res) {                  // "posts" the data entered 
     let url = "https://us21.api.mailchimp.com/3.0/lists/194b59209d";
     let options = {
         method: "POST",
-        auth: "sancti:85fc324b1b7cb9948186f32de11271b5-us21"
+        auth: "admn:035aec3923779fbd5ae2036ca7399685-us21"
     }
 
         
